9225c5ac81f9282e6141db4abc97ddd35f155529
Kenny Collins <kennethcollins@sandiego.edu>
2024-04-03 09:23:25 -0700

    Pushing initial setup

---
+++ proj2.java
@@ -0,0 +1,12 @@
+/* 
+File: proj2.java
+Authors: Kenny Collins, Anthony Rojas, Scott Schnieders, Rakan Al rasheed
+Date: 4/2/2024
+Description: This is the driver file of our program.
+*/
+
+public class proj2{
+    /*
+     * Just filling for now
+     */
+}
