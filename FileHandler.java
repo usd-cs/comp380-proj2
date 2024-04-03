41bde998fc53b7df4f5621fce93f4ca10623ad9a
Kenny Collins <kennethcollins@sandiego.edu>
2024-04-03 09:39:00 -0700

    Should be last necessary files

---
+++ FileHandler.java
@@ -0,0 +1,10 @@
+/* 
+File: FileHandler.java
+Authors: Kenny Collins, Anthony Rojas, Scott Schnieders, Rakan Al rasheed
+Date: 4/2/2024
+Description: This file is responsible for all File Handling Methods.
+*/
+
+public class FileHandler {
+    
+}
---
+++ HopfieldNet.java
@@ -0,0 +1,9 @@
+/* 
+File: HopfieldNet.java
+Authors: Kenny Collins, Anthony Rojas, Scott Schnieders, Rakan Al rasheed
+Date: 4/2/2024
+Description: Responsible for training and testing the Hopfield. Utilizes FileHandler for modularity.
+*/
+public class HopfieldNet {
+    
+}
