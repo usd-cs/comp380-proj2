6ea7f8e79452180742ad333bca8fda231e7eefbc
Kenny Collins <kennethcollins@sandiego.edu>
2024-04-06 19:54:16 -0700

    FileHandler pretty much done

--- HopfieldNet.java
+++ HopfieldNet.java
@@ -1,13 +1,31 @@
 /* 
 File: HopfieldNet.java
 Authors: Kenny Collins, Anthony Rojas, Scott Schnieders, Rakan Al rasheed
-Date: 4/2/2024
+Date: 4/6/2024
 Description: Responsible for training and testing the Hopfield. Utilizes FileHandler for modularity.
 */
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class HopfieldNet {
+
+    private class Hopfield{
+        // Class for loading/storing Net
+        int numDimensions;
+        int capacity;
+        ArrayList<ArrayList<ArrayList<Integer>>> storedPatterns;
+        ArrayList<ArrayList<Integer>> weights;
+
+        public Hopfield(int numDimensions, int capacity, 
+                        ArrayList<ArrayList<ArrayList<Integer>>> storedPatterns,
+                        ArrayList<ArrayList<Integer>> weights){
+                        this.numDimensions = numDimensions;
+                        this.capacity = capacity;
+                        this.storedPatterns = storedPatterns;
+                        this.weights = weights;
+                        }
+    }
     public void train(String trainingDataFile, String weightSettingsFile) throws FileNotFoundException {
         /*
         * Notes for training:
@@ -15,12 +33,10 @@ public class HopfieldNet {
         * threshold is typically zero for hebb nett, and binary inputs are used.
         * Pretty straightforward.
         */
+        Hopfield hopNet = new Hopfield(0, 0, null, null);
     }
     public void test(String testingDataFile, String resultsFile){
         // TODO: Implement, shouldn't be too bad
-    }
-    public void loadWeights(String weightSettingsFile){
-        // TODO: Call inside test Method
     }
 
 }
--- proj2.java
+++ proj2.java
@@ -1,7 +1,7 @@
 /* 
 File: proj2.java
 Authors: Kenny Collins, Anthony Rojas, Scott Schnieders, Rakan Al rasheed
-Date: 4/2/2024
+Date: 4/6/2024
 Description: This is the driver file of our program.
 */
 import java.io.IOException;
@@ -65,5 +65,6 @@ public class proj2{
 
             }
         }
+        kb.close();
     }
 }
